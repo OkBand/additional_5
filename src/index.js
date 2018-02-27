77f06264ac8de9510d2c5d8012763137e791552d
OkBand <o.bandura42@gmail.com>
2018-02-27 23:36:19 +0300

    initial

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  var array = [];
+  var l = str.length;
+  var i, symbol, expected;
+
+  var opening = ['[', '{', '(', '|', '1', '3', '5', '7', '8'];
+  var closing = [']', '}', ')', '|' '2', '4', '6', '7', '8'];
+
+  for (i = 0; i < l; i++) {
+    symbol = str[i];
+
+    if (opening.indexOf(symbol) > -1) {
+      array.push(symbol);
+    }
+    else if (closing.indexOf(symbol) > -1) {
+      expected = opening[closing.indexOf(symbol)];
+        if (array.length === 0 || (array.pop() !== expected)) {
+        return false;
+      };
+    };
+};
+};
